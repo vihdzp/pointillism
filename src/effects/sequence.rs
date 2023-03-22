c74d36282c20198cc50907cbcc2b22c68b87c7c9
Violeta Hernández <vi.hdz.p@gmail.com>
2023-03-22 14:00:25 -0600

    loop golfing

--- src/effects/sequence.rs
+++ src/effects/sequence.rs
@@ -26,7 +26,7 @@ pub struct Sequence<S: Signal, F: Mut<S, Time>> {
 
 impl<S: Signal, F: Mut<S, Time>> Sequence<S, F> {
     /// Initializes a new sequence.
-    pub fn new(times: Vec<Time>, sgn: S, func: F) -> Self {
+    pub const fn new(times: Vec<Time>, sgn: S, func: F) -> Self {
         Self {
             times,
             sgn,
@@ -38,22 +38,22 @@ impl<S: Signal, F: Mut<S, Time>> Sequence<S, F> {
     }
 
     /// The current event index.
-    pub fn idx(&self) -> usize {
+    pub const fn idx(&self) -> usize {
         self.idx
     }
 
     /// Time since last event.
-    pub fn since(&self) -> Time {
+    pub const fn since(&self) -> Time {
         self.since
     }
 
     /// Time since instantiation.
-    pub fn total(&self) -> Time {
+    pub const fn total(&self) -> Time {
         self.total
     }
 
     /// Returns a reference to the modified signal.
-    pub fn sgn(&self) -> &S {
+    pub const fn sgn(&self) -> &S {
         &self.sgn
     }
 
@@ -118,86 +118,50 @@ impl<S: Signal, F: Mut<S, Time>> Signal for Sequence<S, F> {
 /// Loops a list of events.
 #[derive(Clone, Debug)]
 pub struct Loop<S: Signal, F: Mut<S, Time>> {
-    /// A list of time intervals between an event and the next.
-    pub times: Vec<Time>,
-
-    /// A signal being modified.
-    pub sgn: S,
-
-    /// The function modifying the signal.
-    pub func: F,
-
-    /// The current event being read.
-    idx: usize,
-
-    /// Time since last event.
-    since: Time,
-
-    /// Time that has passed since instantiation.
-    total: Time,
+    /// The internal sequence.
+    pub seq: Sequence<S, F>,
 }
 
 impl<S: Signal, F: Mut<S, Time>> Loop<S, F> {
-    /// Initializes a new sequence.
-    pub fn new(times: Vec<Time>, sgn: S, func: F) -> Self {
-        Self {
-            times,
-            sgn,
-            func,
-            idx: 0,
-            since: Time::ZERO,
-            total: Time::ZERO,
-        }
+    /// Initializes a new loop from a sequence.
+    pub const fn new_seq(seq: Sequence<S, F>) -> Self {
+        Self { seq }
+    }
+
+    /// Initializes a new loop.
+    pub const fn new(times: Vec<Time>, sgn: S, func: F) -> Self {
+        Self::new_seq(Sequence::new(times, sgn, func))
     }
 
     /// The current event index.
-    pub fn idx(&self) -> usize {
-        self.idx
+    pub const fn idx(&self) -> usize {
+        self.seq.idx
     }
 
     /// Time since last event.
-    pub fn since(&self) -> Time {
-        self.since
+    pub const fn since(&self) -> Time {
+        self.seq.since
     }
 
     /// Time since instantiation.
-    pub fn total(&self) -> Time {
-        self.total
+    pub const fn total(&self) -> Time {
+        self.seq.total
     }
 
     /// Returns a reference to the modified signal.
-    pub fn sgn(&self) -> &S {
-        &self.sgn
+    pub const fn sgn(&self) -> &S {
+        &self.seq.sgn
     }
 
     /// Returns a mutable reference to the modified signal.
     pub fn sgn_mut(&mut self) -> &mut S {
-        &mut self.sgn
+        &mut self.seq.sgn
     }
 
     /// The number of events in the loop.
     #[allow(clippy::len_without_is_empty)]
     pub fn len(&self) -> usize {
-        self.times.len()
-    }
-
-    /// Attempts to read a single event, returns whether it was successful.
-    fn read_event(&mut self) -> bool {
-        let event_time = self.times[self.idx() % self.len()];
-        let read = self.since() >= event_time;
-
-        if read {
-            self.since -= event_time;
-            self.func.modify(&mut self.sgn, self.total);
-            self.idx += 1;
-        }
-
-        read
-    }
-
-    /// Reads all current events.
-    fn read_events(&mut self) {
-        while self.read_event() {}
+        self.seq.times.len()
     }
 }
 
@@ -205,20 +169,18 @@ impl<S: Signal, F: Mut<S, Time>> Signal for Loop<S, F> {
     type Sample = S::Sample;
 
     fn get(&self) -> Self::Sample {
-        self.sgn.get()
+        self.seq.sgn.get()
     }
 
     fn advance(&mut self) {
-        self.sgn.advance();
-        self.since.advance();
-        self.total.advance();
-        self.read_events();
+        self.seq.advance();
+
+        if self.seq.idx > self.len() {
+            self.seq.idx = 0;
+        }
     }
 
     fn retrigger(&mut self) {
-        self.sgn.retrigger();
-        self.idx = 0;
-        self.since = Time::ZERO;
-        self.total = Time::ZERO;
+        self.seq.retrigger();
     }
 }
