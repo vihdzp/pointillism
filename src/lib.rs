fad4074aae72b63fc9f445f065e245f2079ac074
Violeta Hernández <vi.hdz.p@gmail.com>
2023-04-30 23:56:40 -0600

    version bump

--- src/lib.rs
+++ src/lib.rs
@@ -47,7 +47,7 @@
 //! The following versions of `pointillism` exist:
 //!
 //! - 0.1.0 - 0.1.7: very early versions, have been yanked from `crates`.
-//! - 0.2.0 - 0.2.7: more stable versions, but still subject to drastic change.
+//! - 0.2.0 - 0.2.8: more stable versions, but still subject to drastic change.
 //!
 //! Once the basic structure of `pointillism` stabilizes, the version will
 //! advance to 0.3.0, and a changelog will be made.
